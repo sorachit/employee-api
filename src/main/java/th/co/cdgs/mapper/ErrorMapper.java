567fa1958bbc558075e0b0d98157bb06d88e58a2
003935 <sorachit.k@cdg.co.th>
2022-08-11 12:17:08 +0700

    เพิ่มการแปล error

--- src/main/java/th/co/cdgs/mapper/ErrorMapper.java
+++ src/main/java/th/co/cdgs/mapper/ErrorMapper.java
@@ -1,38 +1,56 @@
 package th.co.cdgs.mapper;
 
-import javax.inject.Inject;
-import javax.ws.rs.WebApplicationException;
+import java.sql.SQLException;
+import java.sql.SQLIntegrityConstraintViolationException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.persistence.OptimisticLockException;
+import javax.persistence.PersistenceException;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ObjectNode;
-import org.jboss.logging.Logger;
+
+import io.quarkus.logging.Log;
 
 @Provider
 public class ErrorMapper implements ExceptionMapper<Exception> {
-    private static final Logger LOGGER = Logger.getLogger(ErrorMapper.class.getName());
-    @Inject
-    ObjectMapper objectMapper;
+    
+    private static final List<Class<?>> expectClazz = Arrays.asList(SQLIntegrityConstraintViolationException.class,OptimisticLockException.class, PersistenceException.class);
+	
+    private Throwable getExpectCause(Throwable throwable, List<Class<?>> classList) {
+		Class<?> clazz = throwable.getClass();
+		if (classList.contains(clazz)) {
+			return throwable;
+		} else {
+			if (throwable.getCause() == null) {
+				return throwable;
+			}
+			return getExpectCause(throwable.getCause(), classList);
+		}
+	}
 
     @Override
     public Response toResponse(Exception exception) {
-        LOGGER.error("Failed to handle request", exception);
+    	
+    	Map<String,String> messages = new HashMap<>();
+    	Throwable childCase = getExpectCause(exception, expectClazz);
+    	if (childCase instanceof SQLException) {
+    		Log.error("getErrorCode = " +((SQLException) childCase).getErrorCode());
+    		messages.put("message", "SQLException code="+((SQLException) childCase).getErrorCode());
+    	}else if (childCase instanceof javax.persistence.OptimisticLockException) {
+    		messages.put("message", "OptimisticLock");
+		} else if (childCase instanceof javax.persistence.PersistenceException
+				&& childCase.getCause() instanceof org.hibernate.exception.ConstraintViolationException) {
+			messages.put("message", "Referential integrity constraint violation");
+		} else {
+			messages.put("message", exception.getMessage());
+		}
 
-        int code = 500;
-        if (exception instanceof WebApplicationException) {
-            code = ((WebApplicationException) exception).getResponse().getStatus();
-        }
-
-        ObjectNode exceptionJson = objectMapper.createObjectNode();
-        exceptionJson.put("exceptionType", exception.getClass().getName());
-        exceptionJson.put("code", code);
-
-        if (exception.getMessage() != null) {
-            exceptionJson.put("error", exception.getMessage());
-        }
-
-        return Response.status(code).entity(exceptionJson).build();
+        return Response.status(Status.INTERNAL_SERVER_ERROR).entity(messages).build();
     }
 
 }
