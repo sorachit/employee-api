9e656bf8ee74ea92940617dbabcc1c31624ffc80
003935 <sorachit.k@cdg.co.th>
2024-09-23 10:31:35 +0700

    จัดบรรทัด

--- src/main/java/th/co/cdgs/auditlog/AuditLog.java
+++ src/main/java/th/co/cdgs/auditlog/AuditLog.java
@@ -1,26 +1,28 @@
 package th.co.cdgs.auditlog;
 
 import java.util.Date;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "audit_log")
 public class AuditLog {
     @Id
+    @Column(name = "tx_date")
     private Date txDate;
-
+    @Column(name = "action")
     private String action;
+    @Column(name = "data")
     private String data;
-
     @PrePersist
     protected void onCreate() {
         this.txDate = new Date(System.currentTimeMillis());
     }
-
     public AuditLog() {
     }
-
     public AuditLog(String action , String data) {
         this.action = action;
         this.data = data;
