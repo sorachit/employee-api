46d1b15ad845a5e14efef11bc1df23b4610748c9
003935 <sorachit.k@cdg.co.th>
2024-09-23 15:31:05 +0700

    เพิ่ม get set

--- src/main/java/th/co/cdgs/employee/EmployeeEmail.java
+++ src/main/java/th/co/cdgs/employee/EmployeeEmail.java
@@ -31,6 +31,14 @@ public class EmployeeEmail {
     @JsonBackReference
     private Employee employee;
 
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    
     public String getEmail() {
         return email;
     }
